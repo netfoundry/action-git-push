08c46af4ff14c1bf6cce8da1d74905359bd78a55
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-02-24 16:48:04 -0500

    rebase temp branch on target branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,16 +13,16 @@ fi
 
 cd "$INPUT_DIRECTORY" || exit 1
 
-CURRENT_BRANCH=$(echo "$GITHUB_REF"|sed -E "s@refs/heads/@@")
 TARGET_BRANCH=$INPUT_BRANCH
 case $TARGET_BRANCH in "refs/heads/"*)
-  TARGET_BRANCH=$(echo  "$TARGET_BRANCH"|sed -E "s@refs/heads/@@")
+  TARGET_BRANCH=$(echo "$TARGET_BRANCH"|sed -E "s@refs/heads/@@")
 esac
 
 if [ "$INPUT_FORCE" != "0" ]; then
   FORCE='--force'
 fi
 
+# https://everything.curl.dev/usingcurl/netrc
 cat <<EOF >| "$HOME/.netrc"
 machine github.com
   login $GITHUB_ACTOR
@@ -36,14 +36,10 @@ EOF
 git config user.email "$INPUT_EMAIL"
 git config user.name "$INPUT_NAME"
 
+git fetch "$INPUT_REPOSITORY" "$GITHUB_REF:actions-x-temp-branch"
+git switch actions-x-temp-branch
 # shellcheck disable=SC2086
 git add $INPUT_FILES -v
 git commit -m "$INPUT_MESSAGE"
-
-git branch actions-x-temp-branch
-git fetch "$INPUT_REPOSITORY" "$CURRENT_BRANCH"
-git checkout "$CURRENT_BRANCH"
-git merge actions-x-temp-branch
-git branch -d actions-x-temp-branch
-
-git push "$INPUT_REPOSITORY" "$CURRENT_BRANCH:$TARGET_BRANCH" "${FORCE:-}"
+git rebase "$TARGET_BRANCH"
+git push "$INPUT_REPOSITORY" "actions-x-temp-branch:$TARGET_BRANCH" "${FORCE:-}"
